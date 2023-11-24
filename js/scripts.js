e40f330abf6c654947074fa063f6ad6b7c3db5fa
Redouane Bekkouche <redouane.bekk@gmail.com>
2023-11-24 19:44:46 +0100

    finished the website

---
+++ js/scripts.js
@@ -0,0 +1,27 @@
+year.textContent = new Date().getFullYear();
+goTop.style.display = 'none';
+let showMobileMenu = false;
+
+function toggleMobileMenu() {
+  if (!showMobileMenu) {
+    mobileMenu.classList.add('show-mobile-menu');
+    mobileMenu.classList.remove('hide-mobile-menu');
+    toggleMenu.src = '../assets/close-menu.svg';
+    showMobileMenu = true;
+  }
+  else {
+    mobileMenu.classList.add('hide-mobile-menu');
+    mobileMenu.classList.remove('show-mobile-menu');
+    toggleMenu.src = '../assets/align-justify.svg';
+    showMobileMenu = false;
+  }
+}
+
+window.onscroll = (evt) => {
+  if (window.scrollY > window.innerHeight) {
+    goTop.style.display = 'block';
+  }
+  else {
+    goTop.style.display = 'none';
+  }
+}
